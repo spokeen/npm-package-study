30b9b1f8c5da6c4eac9647e2b7ab7f03e10e0d20
spokeen <634609252@qq.com>
2022-12-03 17:05:18 +0800

    feat: 匹配器

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,32 @@ const sum = require('./index');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+test('number', () => {
+    const z = 0;
+    expect(z).toBe(0);
+    expect(z).toBeGreaterThan(-1);
+    expect(z).toBeGreaterThanOrEqual(-1);
+    expect(z).toBeLessThan(1);
+    expect(z).toBeLessThanOrEqual(1);
+    expect(0.1 + 0.2).toBeCloseTo(0.3)
+});
+
